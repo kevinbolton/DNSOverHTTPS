263262f16cdaba8844e550a505eece77d6ba286e
Kevin Bolton <kevin_bolton@outlook.com>
2018-08-28 18:26:08 +0800

    delete Mutation Observer

--- FirefoxAddon/content_scripts/check.js
+++ FirefoxAddon/content_scripts/check.js
@@ -1,18 +1,9 @@
-const title = document.querySelector('title');
-const config = { subtree: true, characterData: true, childList: true };
-let observer = new MutationObserver(function(mutations) {
-  change_title();
-});
-
 function change_title() {
   let hostname = `${window.location.hostname}`;
   browser.runtime.sendMessage({"domain_name": hostname});
   if (!document.title.includes(hostname)) {
-    observer.disconnect();
     document.title = document.title + " - " + hostname;
-    observer.observe(title, config);
   }
 }
 
-observer.observe(title, config);
-window.addEventListener ("load", change_title, false);
+window.addEventListener("load", change_title());
