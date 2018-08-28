0a19fda9189c7e2302a50eb3ae314670d36a5ba2
Kevin Bolton <kevin_bolton@outlook.com>
2018-08-28 17:45:19 +0800

    Add promise err function

--- FirefoxAddon/background_script/DoH.js
+++ FirefoxAddon/background_script/DoH.js
@@ -1,13 +1,18 @@
 function handleMessage(message) {
     console.log("Message from the content script: " + message.domain_name);
 
-    var web_site_ip;
-    var web_site_ip_trr;
+//    var web_site_ip;
+//    var web_site_ip_trr;
     
     function resolved(record) {
         console.log("Resolved by OS' DNS...");
         console.log("canonicalName: ", record.canonicalName);
         console.log("IP addresses: ", record.addresses);
+        if (record.isTRR) {
+            console.log("IP is by TRR");
+        } else {
+            console.log("IP is not by TRR");
+        }
         web_site_ip = record.addresses[0];
       }
     
@@ -15,6 +20,11 @@ function handleMessage(message) {
         console.log("Resolved by trr...");
         console.log("canonicalName: ", record_trr.canonicalName);
         console.log("IP addresses: ", record_trr.addresses);
+        if (record_trr.isTRR) {
+            console.log("IP is by TRR");
+        } else {
+            console.log("IP is not by TRR");
+        }
         web_site_ip_trr = record_trr.addresses[0];
         console.log("First IP addresses: ", web_site_ip);
         console.log("First IP_trr addresses: ", web_site_ip_trr);
@@ -53,11 +63,16 @@ function handleMessage(message) {
         }
     }
 
-    let resolving = browser.dns.resolve(message.domain_name, ["disable_trr", "bypass_cache", "canonical_name"]);
-    resolving.then(resolved);
+    function err(record_err) {
+        console.log("DNS resolve Error");
+        console.log(record_err.message);
+    }
+
+    resolving = browser.dns.resolve(message.domain_name, ["disable_trr", "bypass_cache", "disable_ipv6", "canonical_name"]);
+    resolving.then(resolved, err);
     
-    let resolving_trr = browser.dns.resolve(message.domain_name, ["bypass_cache", "canonical_name"]);
-    resolving_trr.then(resolved_trr);
+    resolving_trr = browser.dns.resolve(message.domain_name, ["bypass_cache", "disable_ipv6", "canonical_name"]);
+    resolving_trr.then(resolved_trr, err);
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
