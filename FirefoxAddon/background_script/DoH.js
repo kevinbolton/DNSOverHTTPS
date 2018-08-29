622d90ed57e3b3ce04460fd7c1aed50d2d8ab87b
Kevin Bolton <kevin_bolton@outlook.com>
2018-08-29 17:26:29 +0800

    remove TRR dns, Add DoH by HTTPS request.

--- FirefoxAddon/background_script/DoH.js
+++ FirefoxAddon/background_script/DoH.js
@@ -1,8 +1,5 @@
 function handleMessage(message) {
     console.log("Message from the content script: " + message.domain_name);
-
-//    var web_site_ip;
-//    var web_site_ip_trr;
     
     function resolved(record) {
         console.log("Resolved by OS' DNS...");
@@ -13,66 +10,96 @@ function handleMessage(message) {
         } else {
             console.log("IP is not by TRR");
         }
-        web_site_ip = record.addresses[0];
-      }
-    
-    function resolved_trr(record_trr) {
-        console.log("Resolved by trr...");
-        console.log("canonicalName: ", record_trr.canonicalName);
-        console.log("IP addresses: ", record_trr.addresses);
-        if (record_trr.isTRR) {
-            console.log("IP is by TRR");
-        } else {
-            console.log("IP is not by TRR");
-        }
-        web_site_ip_trr = record_trr.addresses[0];
-        console.log("First IP addresses: ", web_site_ip);
-        console.log("First IP_trr addresses: ", web_site_ip_trr);
-        function changed(changed) {
-            console.log("changed");
-        }
-        function nonchange(changed) {
-            console.log("nonchanged");
-        }
-            
-        if (web_site_ip == web_site_ip_trr) {
-            console.log("Green");
-            var currentTab;
-            function updateIcon() {
-                console.log("Tabid", currentTab.id);
-                browser.browserAction.setIcon({
-                    path: {
-                        16: "icons/GreenButton-16.png",
-                        32: "icons/GreenButton-32.png"
-                    },
-                    tabId: currentTab.id
-                });
-            }
 
-            function updateTab(tabs) {
-                if (tabs[0]) {
-                  currentTab = tabs[0];
-                  updateIcon();
+        function processData(data) {
+            if (data.Status == "0") {
+                for (i in data.Answer) {
+                    console.log("TTR IP ", i, " is: ", data.Answer[i].data);
+                    for (j in record.addresses) {
+                        console.log("IP ", j, " is: ", record.addresses[j]);
+                        if (data.Answer[i].data == record.addresses[j]) {
+                            return true;
+                        }
+                    }
                 }
+                return false;
+            } else {
+                console.log("[Error] DoH status: ", data.Status);    
             }
-            var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
-            gettingActiveTab.then(updateTab);
-        } else {
-            console.log("Red");
-            browser.browserAction.setIcon({path: "icons/RedButton-32.png"});
         }
+
+        function handler() {
+            if(client.status == 200 && client.response != null) {
+                if (processData(client.response)) {
+                    console.log("Green");
+                    var currentTab;
+
+                    function updateIcon() {
+                        console.log("Tabid", currentTab.id);
+                        browser.browserAction.setIcon({
+                            path: {
+                                16: "icons/GreenButton-16.png",
+                                32: "icons/GreenButton-32.png"
+                            },
+                            tabId: currentTab.id
+                        });
+                    }
+        
+                    function updateTab(tabs) {
+                        if (tabs[0]) {
+                          currentTab = tabs[0];
+                          updateIcon();
+                        }
+                    }
+
+                    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+                    gettingActiveTab.then(updateTab);
+                } else {
+                    console.log("Red");
+                    var currentTab;
+
+                    function updateIcon() {
+                        console.log("Tabid", currentTab.id);
+                        browser.browserAction.setIcon({
+                            path: {
+                                16: "icons/RedButton-16.png",
+                                32: "icons/RedButton-32.png"
+                            },
+                            tabId: currentTab.id
+                        });
+                    }
+        
+                    function updateTab(tabs) {
+                        if (tabs[0]) {
+                          currentTab = tabs[0];
+                          updateIcon();
+                        }
+                    }
+                    
+                    var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
+                    gettingActiveTab.then(updateTab);
+                }
+            } else {
+                console.log("[Error] HTTP status: ", client.status);
+                console.log("[Error] Response: ", client.response);
+            }
+        }
+
+        var urlCloudflareDoH = "https://cloudflare-dns.com/dns-query?name=" + message.domain_name + "&type=A&do=false&cd=false";
+        var client = new XMLHttpRequest();
+        client.open("GET", urlCloudflareDoH, true);
+        client.setRequestHeader("accept", "application/dns-json")
+        client.responseType = "json";
+        client.onload = handler;
+        client.send();
     }
 
     function err(record_err) {
-        console.log("DNS resolve Error");
-        console.log(record_err.message);
+        console.log("DNS resolve Error", record_err.message);
     }
 
     resolving = browser.dns.resolve(message.domain_name, ["disable_trr", "bypass_cache", "disable_ipv6", "canonical_name"]);
     resolving.then(resolved, err);
-    
-    resolving_trr = browser.dns.resolve(message.domain_name, ["bypass_cache", "disable_ipv6", "canonical_name"]);
-    resolving_trr.then(resolved_trr, err);
 }
 
 browser.runtime.onMessage.addListener(handleMessage);
