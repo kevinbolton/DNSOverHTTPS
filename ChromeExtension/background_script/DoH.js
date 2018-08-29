059720e0cb740d4785c0377e6f8e9614323f2302
Kevin Bolton <kevin_bolton@outlook.com>
2018-08-29 17:33:41 +0800

    Add all file even not complete

---
+++ ChromeExtension/background_script/DoH.js
@@ -0,0 +1,8 @@
+chrome.devtools.network.onRequestFinished.addListener(
+    function(request) {
+      if (request.response.bodySize > 40*1024) {
+        chrome.devtools.inspectedWindow.eval(
+            'console.log("Large image: " + unescape("' +
+            escape(request.request.url) + '"))');
+      }
+});
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # DNSOverHTTPS
+
+These are PoCs of DoH.
+
+DoH by Python is done.
+DoH by Firefox add-on is doing.
+Doh by Chrome extension is pending.
