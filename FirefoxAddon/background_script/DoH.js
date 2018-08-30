3a9a0c3c72322eb99c52787080c5c09fb760a99b
Kevin Bolton <kevin_bolton@outlook.com>
2018-08-30 18:14:03 +0800

    Recive IP and FQDN from HTTP for compare

--- FirefoxAddon/background_script/DoH.js
+++ FirefoxAddon/background_script/DoH.js
@@ -1,105 +1,147 @@
 function handleMessage(message) {
-    console.log("Message from the content script: " + message.domain_name);
-    
-    function resolved(record) {
-        console.log("Resolved by OS' DNS...");
-        console.log("canonicalName: ", record.canonicalName);
-        console.log("IP addresses: ", record.addresses);
-        if (record.isTRR) {
-            console.log("IP is by TRR");
-        } else {
-            console.log("IP is not by TRR");
-        }
+    function resolved(recordOS) {
+        function validationIP(recordDoH) {
+            if (recordDoH.Status == "0") {
+                console.log("DoH status: NoError");
+                for (let i in recordDoH.Answer) {                    
+                    console.log("TTR IP ", i, " is: ", recordDoH.Answer[i].data);
+                } // Print all IPs from DoH server
+                for (let j in recordOS.addresses) {
+                    console.log("OS'DNS IP ", j, " is: ", recordOS.addresses[j]);
+                } // Print all IPs from OS DNS server
+                console.log("Web IP: ", webIPHTTP); // Print IP from browser
+                console.log("Web URL: ", webFQDNHTTP); // Print URL from browser
 
-        function processData(data) {
-            if (data.Status == "0") {
-                for (i in data.Answer) {
-                    console.log("TTR IP ", i, " is: ", data.Answer[i].data);
-                    for (j in record.addresses) {
-                        console.log("IP ", j, " is: ", record.addresses[j]);
-                        if (data.Answer[i].data == record.addresses[j]) {
+                // Need to make a decision: What is the best way(DOM or HTTP) to get FQDN, and redesign code
+                // Need to make a decision: Should add-on use OS DNS' IP to compare?
+                // If both of FQDNs that received from HTTP & windows are same, compare HTTP'IP & DoH'IP
+                if (message.webFQDN == webFQDNHTTP) {
+                    for (i in recordDoH.Answer) {
+                        if (recordDoH.Answer[i].data == webIPHTTP) {
+                            console.log("TRR IP: ", recordDoH.Answer[i].data, " = ", webIP);
                             return true;
                         }
                     }
+                    return false;
+                } else { // else compare OS DNS IP & DoH'sIP
+                    for (i in recordDoH.Answer) {
+                        for (j in recordOS.addresses) {
+                            if (recordDoH.Answer[i].data == recordOS.addresses[j]) {
+                                console.log("TRR IP: ", recordDoH.Answer[i].data, " = ", recordOS.addresses[j]);
+                                return true;
+                            }
+                        }
+                    }
                 }
-                return false;
             } else {
-                console.log("[Error] DoH status: ", data.Status);    
+                console.log("[Error] DoH status: ", recordDoH.Status);    
             }
         }
 
-        function handler() {
-            if(client.status == 200 && client.response != null) {
-                if (processData(client.response)) {
-                    console.log("Green");
-                    var currentTab;
+        function updateIcon() {
+            if (iconColor = "green") {
+                browser.browserAction.setIcon({
+                    path: {
+                        16: "icons/GreenButton-16.png",
+                        32: "icons/GreenButton-32.png"
+                    },
+                    tabId: currentTab.id
+                });
+            } else if (iconColor = "red") {
+                browser.browserAction.setIcon({
+                    path: {
+                        16: "icons/RedButton-16.png",
+                        32: "icons/RedButton-32.png"
+                    },
+                    tabId: currentTab.id
+                });
+            }
+        }
 
-                    function updateIcon() {
-                        console.log("Tabid", currentTab.id);
-                        browser.browserAction.setIcon({
-                            path: {
-                                16: "icons/GreenButton-16.png",
-                                32: "icons/GreenButton-32.png"
-                            },
-                            tabId: currentTab.id
-                        });
-                    }
-        
-                    function updateTab(tabs) {
-                        if (tabs[0]) {
-                          currentTab = tabs[0];
-                          updateIcon();
-                        }
-                    }
+        function updateTab(tabs) {
+            if (tabs[0]) {
+              currentTab = tabs[0];
+              updateIcon();
+            }
+        }
 
+        function handlerDoH() {
+            if(clientDoH.status == 200 && clientDoH.response != null) {
+                if (validationIP(clientDoH.response)) {
+                    console.log("Change icon to green color");
                     var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
                     gettingActiveTab.then(updateTab);
+                    iconColor = "green"
                 } else {
-                    console.log("Red");
-                    var currentTab;
-
-                    function updateIcon() {
-                        console.log("Tabid", currentTab.id);
-                        browser.browserAction.setIcon({
-                            path: {
-                                16: "icons/RedButton-16.png",
-                                32: "icons/RedButton-32.png"
-                            },
-                            tabId: currentTab.id
-                        });
-                    }
-        
-                    function updateTab(tabs) {
-                        if (tabs[0]) {
-                          currentTab = tabs[0];
-                          updateIcon();
-                        }
-                    }
-                    
+                    console.log("Change icon to red color");                    
                     var gettingActiveTab = browser.tabs.query({active: true, currentWindow: true});
                     gettingActiveTab.then(updateTab);
+                    iconColor = "red"
                 }
             } else {
-                console.log("[Error] HTTP status: ", client.status);
-                console.log("[Error] Response: ", client.response);
+                console.log("[Error] DoH HTTP status: ", clientDoH.status);
+                console.log("[Error] DoH Response: ", clientDoH.response);
             }
         }
 
-        var urlCloudflareDoH = "https://cloudflare-dns.com/dns-query?name=" + message.domain_name + "&type=A&do=false&cd=false";
-        var client = new XMLHttpRequest();
-        client.open("GET", urlCloudflareDoH, true);
-        client.setRequestHeader("accept", "application/dns-json")
-        client.responseType = "json";
-        client.onload = handler;
-        client.send();
+        console.log("Resolved by OS' DNS...");
+        console.log("canonicalName: ", recordOS.canonicalName);
+        console.log("IP addresses: ", recordOS.addresses);
+        console.log("Is TRR?: ", recordOS.isTRR.toString());
+
+        var currentTab;
+        var iconColor;
+        /* Enable when use cloudflare-dns */
+        //var urlDoH = "https://cloudflare-dns.com/dns-query?name=" + message.webFQDN + "&type=A&do=false&cd=false";
+        var urlDoH = "https://dns.google.com/resolve?name=" + message.webFQDN;
+
+        var clientDoH = new XMLHttpRequest();
+        clientDoH.open("GET", urlDoH, true);
+        /* Enable when use cloudflare-dns */
+        // clientDoH.setRequestHeader("accept", "application/dns-json");
+        clientDoH.responseType = "json";
+        clientDoH.onload = handlerDoH;
+        clientDoH.send();
     }
 
-    function err(record_err) {
-        console.log("DNS resolve Error", record_err.message);
+    function err(recordOS_err) {
+        console.log("DNS resolve Error", recordOS_err.message);
     }
 
-    resolving = browser.dns.resolve(message.domain_name, ["disable_trr", "bypass_cache", "disable_ipv6", "canonical_name"]);
+    console.log("Message from the content script: " + message.webFQDN);
+    resolving = browser.dns.resolve(message.webFQDN, ["disable_trr", "bypass_cache", "disable_ipv6", "canonical_name"]);
     resolving.then(resolved, err);
 }
+
+var webIPHTTP;
+var webFQDNHTTP;
+
+function getFQDN(url) {
+    var FQDN;
+
+    if (url.indexOf("//") > -1) {
+        FQDN = url.split('/')[2];
+    }
+    else {
+        FQDN = url.split('/')[0];
+    }
+
+    //find & remove port number
+    FQDN = FQDN.split(':')[0];
+    //find & remove "?"
+    FQDN = FQDN.split('?')[0];
+
+    return FQDN;
+}
+
+browser.webRequest.onResponseStarted.addListener(webResponse => {
+    webIPHTTP = webResponse.ip;
+    webFQDNHTTP = getFQDN(webResponse.url);
+}, {
+    urls: ['<all_urls>'],
+    types: ['main_frame']
+}, [
+    'responseHeaders' // to prevent "No tab with id" error
+]);
 
 browser.runtime.onMessage.addListener(handleMessage);
--- FirefoxAddon/content_scripts/check.js
+++ FirefoxAddon/content_scripts/check.js
@@ -1,9 +1,11 @@
-function change_title() {
-  let hostname = `${window.location.hostname}`;
-  browser.runtime.sendMessage({"domain_name": hostname});
-  if (!document.title.includes(hostname)) {
-    document.title = document.title + " - " + hostname;
+function sendWebFQDN() {
+  let webFQDN = `${window.location.hostname}`;
+  browser.runtime.sendMessage({"webFQDN": webFQDN});
+  /*
+  if (!document.title.includes(webFQDN)) {
+    document.title = document.title + " - " + webFQDN;
   }
+  */
 }
 
-window.addEventListener("load", change_title());
+window.addEventListener("load", sendWebFQDN());
